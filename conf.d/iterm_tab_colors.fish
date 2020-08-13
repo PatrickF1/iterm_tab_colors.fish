0d5dc5d32170aaa0cfca353c2ff351b0873a1a58
Patrick Fong <patrickf3139@gmail.com>
2020-08-13 08:30:48 -0700

    don't differentiate tab colors if not interactive shell

--- conf.d/iterm_tab_colors.fish
+++ conf.d/iterm_tab_colors.fish
@@ -1,2 +1,4 @@
-# run function that changes tab color on every shell start
-__differentiate_tab_colors
+# run function that changes tab color on every shell start unless not interactive
+if status --is-interactive
+    __differentiate_tab_colors
+end
