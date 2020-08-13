46e644796fc5281206780ee3ec4828d04e25c33c
Patrick Fong <patrickf3139@gmail.com>
2020-08-13 08:44:56 -0700

    erase universal variable on uninstall

--- conf.d/iterm_tab_colors.fish
+++ conf.d/iterm_tab_colors.fish
@@ -2,3 +2,13 @@
 if status --is-interactive
     __differentiate_tab_colors
 end
+
+function iterm_tab_colors_uninstall --on-event iterm_tab_colors_uninstall
+    # __tab_color_index is the universal variable used to track which color to use
+    # for the next tab
+    set --erase __tab_color_index
+
+    set_color --italics cyan
+    echo "iterm_tab_colors.fish uninstalled"
+    set_color normal
+end
