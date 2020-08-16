2642ca3f9c6ff5e15a19313b18eb9558f8474d66
Patrick Fong <patrickf3139@gmail.com>
2020-08-16 09:14:15 -0700

    don't do anything if current terminal isn't iterm2

--- functions/__differentiate_tab_colors.fish
+++ functions/__differentiate_tab_colors.fish
@@ -1,5 +1,14 @@
 #!/usr/local/bin/fish
 function __differentiate_tab_colors -d "set the current iTerm2 tab to the next color in a sequence of colors"
+    # The standard way of installing iterm2 utilities for fish doesn't make them available to conf.d scripts
+    # at their point of evaluation (see https://fishshell.com/docs/current/#initialization-files for why)
+    # so we add the utilities package to PATH so we can execute them.
+    set --prepend PATH "$HOME/.iterm2"
+
+    if not it2check
+        return
+    end
+
     set red ff0d00
     set green 1f8705
     set yellow ffee03
@@ -17,7 +26,6 @@ function __differentiate_tab_colors -d "set the current iTerm2 tab to the next c
     end
     # need to add 1 to __tab_color_index fish arrays indices start at 1
     set color_index (math "$__tab_color_index + 1")
-    # Depending on how the user installed iterm2 utilities, iterm2 may not yet be available
-    # so just execute it directly.
-    ~/.iterm2/it2setcolor tab $colors_array[$color_index]
+
+    it2setcolor tab $colors_array[$color_index]
 end
